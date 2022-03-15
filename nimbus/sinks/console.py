8c2890e37b6e294efaa760f7fdf3881b43ca9537
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-14 21:33:28 -0500

    Created Console sink

---
+++ nimbus/sinks/console.py
@@ -0,0 +1,13 @@
+import numpy.typing as npt
+
+
+class Console:
+    """Console is a sink that prints a signal to the console"""
+
+    def __init__(self):
+        pass
+
+    def console_print(self, signal: npt.NDArray):
+        """Prints signal to console"""
+        print(signal)
+        return None
