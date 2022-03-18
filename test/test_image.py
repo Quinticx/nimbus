3ef6c66f69b2a02d66a085d31350e962358e2a9e
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-18 17:34:24 -0500

    Added test_image.py

---
+++ test/test_image.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+import pytest as pt
+from nimbus.sources import Image as ims
+from nimbus.sinks import Image as imf
+
+
+def test_imagefile():
+    i_source = ims()
+    imarray = i_source.open("bri.jpg")
+
+    i_sink = imf()
+    i_sink.execute(imarray)
