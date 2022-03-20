34bc9216f88c375a5410c8c425d3bb66f1d5aa38
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-20 12:46:37 -0500

    Fixed end-to-end Wave test

--- test/test_wave.py
+++ test/test_wave.py
@@ -8,20 +8,31 @@ from nimbus.transformers import Gain
 from nimbus import Pipeline
 from test.mocks import MockSink, MockSource
 
-"""
-def test_wavefile():
+
+def test_wavefile(tmp_path):
     samplerate = 44100
     fs = 100
     t = np.linspace(0.0, 1.0, samplerate)
     amplitude = np.iinfo(np.int16).max
     data = amplitude * np.sin(2.0 * np.pi * fs * t)
-    data = data.astype(np.int16)
-    test_sink = wf()
-    test_sink.write(data)
-    test_source = ws("output.wav")
-    test_signal, frame_rate = test_source.read()
-    assert_array_equal(data, test_signal)
-"""
+    expected_data = data.astype(np.int16)
+    filename = tmp_path / "test_wave.wav"
+
+    test_source = MockSource(expected_data)
+    wave_sink = wf(filename, samplerate)
+
+    pipeline1 = Pipeline(test_source, [Gain(1)], wave_sink)
+    pipeline1.run()
+
+    wave_source = ws(filename)
+    test_sink = MockSink()
+
+    pipeline2 = Pipeline(wave_source, [Gain(1)], test_sink)
+    pipeline2.run()
+
+    actual_data = test_sink.get_buffer()
+
+    assert_array_equal(expected_data, actual_data)
 
 
 def test_wavesink(tmp_path):
@@ -33,8 +44,8 @@ def test_wavesink(tmp_path):
     expected_data = expected_data.astype(np.int16)
     filename = tmp_path / "test_wave.wav"
 
-    test_source = MockSource(expected_data, samplerate)
-    test_sink = wf(filename)
+    test_source = MockSource(expected_data)
+    test_sink = wf(filename, samplerate)
 
     pipeline = Pipeline(test_source, [Gain(1)], test_sink)
     pipeline.run()
