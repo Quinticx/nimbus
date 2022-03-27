cb66f177ecc5f99bdd9b4b3f089c82945c3450f2
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-27 17:28:36 -0500

    Added IQ examples

---
+++ examples/iq_to_audio.py
@@ -0,0 +1,21 @@
+import nimbus
+import nimbus.sources
+import nimbus.sinks
+import nimbus.transformers
+import numpy as np
+from pathlib import Path
+
+iq_source = nimbus.sources.IQ(Path("examples/test_iq.iq"))
+audio_sink = nimbus.sinks.Audio()
+console_sink = nimbus.sinks.Console()
+
+pipeline = nimbus.Pipeline(
+    iq_source,
+    [
+        nimbus.transformers.FM_Demod(),
+        nimbus.transformers.Resample(44700),
+        nimbus.transformers.Gain(2),
+    ],
+    audio_sink,
+)
+pipeline.run()
