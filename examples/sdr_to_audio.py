e1d2fa6698bcb07e336af392b5c6704766fe5f98
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-27 02:15:13 -0500

    Added sdr_to_audio example

---
+++ examples/sdr_to_audio.py
@@ -0,0 +1,20 @@
+import nimbus
+import nimbus.sources
+import nimbus.sinks
+import nimbus.transformers
+import numpy as np
+
+sdr_source = nimbus.sources.SDR(sample_rate=2.56e5, frequency=9.91e7)
+audio_sink = nimbus.sinks.Audio()
+console_sink = nimbus.sinks.Console()
+
+pipeline = nimbus.Pipeline(
+    sdr_source,
+    [
+        nimbus.transformers.FM_Demod(),
+        nimbus.transformers.Resample(44700),
+        nimbus.transformers.Gain(2)
+        ],
+    audio_sink,
+)
+pipeline.run()
