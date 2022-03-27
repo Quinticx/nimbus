0676daf2d066138eefd73b25030ea4eec3788aac
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-27 00:58:38 -0500

    Fixed black

--- nimbus/sinks/audio.py
+++ nimbus/sinks/audio.py
@@ -2,8 +2,9 @@ import pyaudio
 import wave
 from nimbus import Samples
 
+
 class Audio:
-    '''Audio is a sink that plays a wave file'''
+    """Audio is a sink that plays a wave file"""
 
     def __init__(self):
         self.stream = None
@@ -11,5 +12,7 @@ class Audio:
     def execute(self, signal: Samples):
         if self.stream == None:
             p = pyaudio.PyAudio()
-            self.stream = p.open(format=pyaudio.paInt16, channels=1, rate=signal.sample_rate, output=True)
+            self.stream = p.open(
+                format=pyaudio.paInt16, channels=1, rate=signal.sample_rate, output=True
+            )
         self.stream.write(signal.data.tobytes())
