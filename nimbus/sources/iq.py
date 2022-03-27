5c68a213e1c074ea360f84bcf157856b3ec6c074
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-27 17:26:56 -0500

    Added IQ Source and Sink

---
+++ nimbus/sources/iq.py
@@ -0,0 +1,24 @@
+import numpy as np
+from nimbus import Samples
+import pathlib
+import json
+
+class IQ:
+    '''IQ is a source that reads from an .iq file and returns a numpy array'''
+
+    def __init__(self, filename: pathlib.Path, buffer_size: int = 2048):
+        self.filename = filename
+        self.iqfile = open(self.filename, 'rb')
+        self.buffer_size = buffer_size
+        jsonfile = open(filename.with_suffix('.json'))
+        info = json.load(jsonfile)
+        self.sample_rate = info['sample_rate']
+
+
+    def read(self):
+        '''Read .iq file and return signal'''
+        data = self.iqfile.read(self.buffer_size)
+        if not data:
+            raise IndexError()
+        data = np.frombuffer(data, dtype=np.complex64)
+        return Samples(data=data, sample_rate=self.sample_rate)
---
+++ test/test_iq.py
@@ -0,0 +1,28 @@
+from nimbus.sinks import IQ as iqf
+from nimbus.sources import IQ as iqs
+from nimbus import Pipeline, Samples
+from test.mocks import MockSink, MockSource
+import numpy as np
+from numpy.testing import assert_array_equal
+import pytest as pt
+
+def test_iqfile(tmp_path):
+    filename = tmp_path / "test_iq.iq"
+    expected_data = np.random.random(10) + np.random.random(10)*1j
+    expected_data = expected_data.astype(np.complex64) 
+    test_source = MockSource(expected_data)
+    iq_sink = iqf(filename)
+
+    pipeline1 = Pipeline(test_source, [], iq_sink)
+    pipeline1.run()
+
+    iq_source = iqs(filename)
+    test_sink = MockSink()
+
+    pipeline2 = Pipeline(iq_source, [], test_sink)
+    pipeline2.run()
+
+    actual_data = test_sink.get_buffer()
+
+    assert_array_equal(expected_data, actual_data)
+
