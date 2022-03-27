ace19b1d71ccf0cb3b0130b92c88b4c6afbbed3e
Brianna Witherell <witherellbrianna@gmail.com>
2022-03-27 01:37:38 -0500

    Added SDR Source

---
+++ nimbus/sources/sdr.py
@@ -0,0 +1,17 @@
+from rtlsdr import RtlSdr
+from nimbus import Samples
+
+class SDR:
+''' SDR is a source that reads from an SDR connected to the computer'''
+
+    def __init__(self, sample_rate: int, frequency: int):
+        self.sdr = RtlSdr()
+        self.sdr.sample_rate = sample_rate
+        self.sdr.center_freq = frequency
+        self.sdr.freq_correction = 60
+        self.sdr.gain = 'auto'
+
+    def read(self):
+        num_samp = 512
+        samples = self.sdr.read_samples(num_samp)
+        return Samples(data=samples, sample_rate = int(self.sdr.sample_rate))
