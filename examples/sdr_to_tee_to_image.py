6c7826617e8ab4efb6842fb553f82e867a95d3a3
Brianna Witherell <witherellbrianna@gmail.com>
2022-04-10 20:33:44 -0500

    Updated examples

--- examples/sdr_to_tee_to_image.py
+++ examples/sdr_to_tee_to_image.py
@@ -4,6 +4,46 @@ import nimbus.sinks
 import nimbus.transformers
 from pathlib import Path
 import numpy as np
+sync_frame = np.array(
+    [
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+    ]
+)
 
 sdr_source = nimbus.sources.SDR(sample_rate=2.56e5, frequency=137.62e6)
 image_sink = nimbus.sinks.Image("examples/noaa.png")
@@ -19,6 +59,7 @@ pipeline = nimbus.Pipeline(
         nimbus.transformers.Caster(np.int16),
         nimbus.transformers.Tee(wave_sink),
         nimbus.transformers.Resample(sample_rate=4160),
+        nimbus.transformers.Caster(np.float32),
         nimbus.transformers.Apt_Sync(sync_frame),
         nimbus.transformers.Caster(np.uint8),
     ],
