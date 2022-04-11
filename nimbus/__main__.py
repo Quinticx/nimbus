466556e0e52deff6cfe126e9ec04b8299cb6a08a
Brianna Witherell <witherellbrianna@gmail.com>
2022-04-10 22:41:16 -0500

    Added help sections for wrapper

--- nimbus/__main__.py
+++ nimbus/__main__.py
@@ -5,15 +5,35 @@ import nimbus.poes
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Nimbus is a tool for INSERT DESC HERE, DO NOT FORGET"
+        description="Nimbus is a tool for processing satellite data"
     )
     parser.add_argument(
-        "source", nargs="?", type=pathlib.Path, help="where to process data from"
+        "source",
+        nargs="?",
+        type=pathlib.Path,
+        help="Where to process data from. Can be an .iq or .wav file. If ommitedd, the SDR is assumed to be the source",
     )
-    parser.add_argument("--wave", dest="wave_file", type=pathlib.Path)
-    parser.add_argument("--iq", dest="iq_file", type=pathlib.Path)
-    parser.add_argument("--image", dest="image_file", type=pathlib.Path)
-    parser.add_argument("--audio", action="store_true")
+    parser.add_argument(
+        "--wave",
+        dest="wave_file",
+        type=pathlib.Path,
+        help="Path to save AM intermediate signal to. If omitted, signal is not saved",
+    )
+    parser.add_argument(
+        "--iq",
+        dest="iq_file",
+        type=pathlib.Path,
+        help="Path to save unprocessed signal to. If omitted, signal is not saved",
+    )
+    parser.add_argument(
+        "--image",
+        dest="image_file",
+        type=pathlib.Path,
+        help="Path to save final output signal to. If omitted, signal is not saved",
+    )
+    parser.add_argument(
+        "--audio", action="store_true", help="Preview audio while processing"
+    )
     parser.add_argument(
         "--sat",
         default="15",
